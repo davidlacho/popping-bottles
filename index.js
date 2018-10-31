2e61e9c4854db76d8a1737b408b1b4ab5cdfad61
David Lacho <david.lacho@gmail.com>
2018-10-31 16:43:55 -0700

    takes process.argv & once the amount has been given, it provides a breakdown

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const buyBottlesWithBottleCaps = bottlePops => Math.floor(bottlePops / 4);
 let totalBottles = 0;
 let bottlesOnHand = 0;
 let bottleCapsOnHand = 0;
+let bottlesObtainedThroughCaps = 0;
+let bottlesObtainedThroughBottles = 0;
 
 const popBottles = (cash) => {
   // First buy the bottles.
@@ -16,6 +18,7 @@ const popBottles = (cash) => {
 
   if (bottleCapsOnHand >= 4) {
     const newBottlesFromCaps = buyBottlesWithBottleCaps(bottleCapsOnHand);
+    bottlesObtainedThroughCaps += newBottlesFromCaps;
     bottlesOnHand += newBottlesFromCaps;
     totalBottles += newBottlesFromCaps;
     bottleCapsOnHand -= (newBottlesFromCaps * 4);
@@ -24,6 +27,7 @@ const popBottles = (cash) => {
 
   if (bottlesOnHand >= 2) {
     const newBottles = buyBottlesCashOrBottles(bottlesOnHand);
+    bottlesObtainedThroughBottles += newBottles;
     totalBottles += newBottles;
     bottlesOnHand -= newBottles;
     bottleCapsOnHand += newBottles;
@@ -34,6 +38,14 @@ const popBottles = (cash) => {
   }
 };
 
-popBottles(10);
-
-console.log(totalBottles);
+if (process.argv[2] && process.argv.length === 3) {
+  popBottles(process.argv[2]);
+  console.log(
+    `TOTAL BOTTLES: ${totalBottles}
+  TOTAL EARNED:
+    BOTTLES:  ${bottlesObtainedThroughBottles}
+    CAPS:     ${bottlesObtainedThroughCaps}`
+  );
+} else {
+  console.log('node index.js <cash>');
+}
